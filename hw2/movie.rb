98ff1afc2c08a50a92e84420611e3a7b6bde32b3
saasbook <saasbook@saasbook.(none)>
2014-06-04 22:51:17 -0300

    HW2 added

---
+++ hw2/movie.rb
@@ -0,0 +1,7 @@
+class Movie < ActiveRecord::Base
+  attr_accessible :title, :rating, :description, :release_date
+  
+  def self.ratings
+    return ['G','PG','PG-13','R']
+  end
+end
---
+++ hw2/movies_controller.rb
@@ -0,0 +1,60 @@
+class MoviesController < ApplicationController
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+  	@all_ratings = Movie.ratings
+  	
+  	filter = params[:sorter] || session[:sorter]
+  	ratings = params[:ratings] || session[:ratings] || Movie.ratings.each_with_object({}) { |i,arr| arr[i] = '1' }
+  	session[:sorter] = filter
+  	session[:ratings] = ratings
+  	
+  	if filter == "title"
+  		@movies = Movie.find(:all, :conditions => { :rating => ratings.keys}, :order => "title")
+  		@headerTitleClass = "hilite"
+  	elsif filter == "release_date"
+  		@movies = Movie.find(:all, :conditions => { :rating => ratings.keys}, :order => "release_date")
+  		@headerDateClass = "hilite"
+  	else
+    	@movies = Movie.find(:all, :conditions => { :rating => ratings.keys})  	
+  	end
+  	if (params[:sorter] == nil && session[:sorter]) || (params[:ratings] == nil && session[:ratings])
+  		flash.keep
+  		redirect_to movies_path :sorter => session[:sorter], :ratings => session[:ratings]
+  	end
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+end
