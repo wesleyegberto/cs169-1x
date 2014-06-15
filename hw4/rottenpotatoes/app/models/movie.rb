2a813fc7a57792d46a3af016619a87e2f97e7958
saasbook <saasbook@saasbook.(none)>
2014-06-15 02:36:15 -0300

    Added HW4 - Final

---
+++ hw4/rottenpotatoes/app/models/movie.rb
@@ -0,0 +1,12 @@
+class Movie < ActiveRecord::Base
+
+  attr_accessible :title, :rating, :description, :release_date, :director
+
+  def self.all_ratings
+    %w(G PG PG-13 NC-17 R)
+  end
+
+  def self.find_by_director(director)
+    Movie.where(:director => director)
+  end
+end
